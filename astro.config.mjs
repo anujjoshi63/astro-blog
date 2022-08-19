c7c3104e180779cdf195bc3375724a8afd17a69f
Anuj <anujjoshi63@gmail.com>
2022-08-19 17:18:59 +0530

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	site: 'https://example.com',
 	integrations: [mdx(), sitemap(), react()],
+	adapter: vercel()
 });
