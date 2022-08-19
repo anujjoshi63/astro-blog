238f10fa9af1f01eee57a6dc8c9b1e15431d1c19
Anuj <anujjoshi63@gmail.com>
2022-08-19 17:42:13 +0530

    astro iamge

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const Article = ({ pageData: pd }) => {
-	console.log('hey', pd);
 	if (pd.error) {
 		pd = { title: 'page not found', body_html: '<h1>NAI' };
 	}
 	return (
 		<>
-			<div>{pd.title}</div>
+			<h1>{pd.title}</h1>
+			<image
+				src={pd.cover_image || 'https://via.placeholder.com/350x150'}
+				alt="This is a wonderful image"
+				height={500}
+				width={500}
+			/>
 			<div
 				dangerouslySetInnerHTML={{
 					__html: pd.body_html
